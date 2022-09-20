906d3a633948a767d87c58b33c205552c51663d0
Tristan Montemitro <tmontemitro@gmail.com>
2022-09-20 10:58:47 +0200

    fonctionnalité créer review  terminée

--- litreview/review/forms.py
+++ litreview/review/forms.py
@@ -12,3 +12,9 @@ class PhotoForm(forms.ModelForm):
     class Meta:
         model = models.Photo
         fields = ['image', 'caption']
+    
+class ReviewForm(forms.ModelForm):
+    edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    class Meta:
+        model = models.Review
+        fields = ['headline', 'rating', 'body']
--- litreview/review/views.py
+++ litreview/review/views.py
@@ -25,7 +25,7 @@ def create_ticket(request):
             ticket = ticket_form.save(commit=False)
             ticket.photo = photo
             ticket.save()
-            ticket.contributors.add(request.user, through_defaults={'contribution': 'Auteur principal'})
+            #ticket.contributors.add(request.user, through_defaults={'contribution': 'Auteur principal'})
             return redirect('posts')
 
     context = {
@@ -37,7 +37,34 @@ def create_ticket(request):
 
 @login_required
 def create_review(request):
-    return render(request, 'review/create_review.html')
+    ticket_form = forms.TicketForm()
+    photo_form = forms.PhotoForm()
+    review_form = forms.ReviewForm()
+    if request == "POST":
+        ticket_form = forms.TicketForm(request.POST)
+        photo_form = forms.PhotoForm(request.POST, request.FILES)
+        review_form = forms.ReviewForm(request.POST)
+        if all([ticket_form.is_valid(),
+                photo_form.is_valid(),
+                review_form.is_valid()]):
+                photo = photo_form(commit=False)
+                photo.uploader = request.user
+                photo.save()
+                ticket = ticket_form.save(commit=False)
+                ticket.photo = photo
+                ticket.save()
+                review = review_form.save(commit=False)
+                review.ticket = ticket
+                review.save()
+                #ticket.contributors.add(request.user, through_defaults={'contribution': 'Auteur principal'})
+                return redirect('posts')
+    context = {
+        'ticket_form': ticket_form,
+        'photo_form': photo_form,
+        'review_form': review_form,
+    }
+
+    return render(request, 'review/create_review.html', context=context)
 
 @login_required
 def display_posts(request):
